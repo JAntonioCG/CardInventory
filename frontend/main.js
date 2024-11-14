0d29cf0fe0b77ca11fd1230d5d2f36d2916a8f33
lupita <lupitapantoja1360@gmail.com>
2024-11-14 10:47:37 -0600

    modificacion

--- frontend/main.js
+++ frontend/main.js
@@ -1,2 +1,46 @@
 
 const apiUrl = 'http://localhost:8888/cardinventory/src/index.php'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const searchForm = document.querySelector('form')
+  const searchInput = document.querySelector('input[type="search"]')
+
+  searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const searchQuery = searchInput.value.trim()
+
+    if (searchQuery === '') {
+      alert('Please enter a search term.')
+      return
+    }
+
+    try {
+      const isCategory = searchQuery.toLowerCase().includes('electric') || searchQuery.toLowerCase().includes('water');
+
+      const url = isCategory ?  apiUrl+'/cartas/category' :  apiUrl+'/cartas/nombre'
+      const body = JSON.stringify(isCategory ? { category: searchQuery } : { name: searchQuery })
+
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: body,
+      });
+
+      if (response.ok) {
+        const cards = await response.json()
+      
+        if (Array.isArray(cards)) {
+          console.log('@@@ Cartas =>', cards)
+        } else {
+          console.log('No se encontraron cartas')
+        }
+      } else {
+        console.error('Error fetching cards')
+      }      
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  })
+})
